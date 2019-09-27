71a7eda80ace50c32483f9bcf889243c8aaebbde
Jake Gambino <jake.gambino1@gmail.com>
2019-09-27 10:53:19 -0500

    init

---
+++ SuperHero/Models/SuperHero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHero.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryPower { get; set; }
+        public string SecondaryPower { get; set; }
+        public string Catchphrase { get; set; }
+
+        public SuperHero()
+        {
+
+        }
+    }
+}
---
+++ SuperHero/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperHero.Startup))]
+namespace SuperHero
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
