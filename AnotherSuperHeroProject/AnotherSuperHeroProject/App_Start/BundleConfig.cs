73d2078ec247fdd05fd8ae758a73d6c65a026b08
Jake Gambino <jake.gambino1@gmail.com>
2019-09-30 08:48:18 -0500

    fixed bootstrap theme issue

--- AnotherSuperHeroProject/AnotherSuperHeroProject/App_Start/BundleConfig.cs
+++ AnotherSuperHeroProject/AnotherSuperHeroProject/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace AnotherSuperHeroProject
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cyborg-3.css",
                       "~/Content/site.css"));
         }
     }
